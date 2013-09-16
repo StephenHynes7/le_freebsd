eaea2405b008b35a0de06ed13a7a3863bc2355f2
StephenHynes7 <stephen@logentries.com>
2013-09-16 10:08:33 +0100

    added install script

---
+++ install_le.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+printf "Downloading nessceary files"
+curl -O https://raw.github.com/logentries/le/master/le
+curl -O https://raw.github.com/StephenHynes7/le_freebsd/master/le.sh
+chmod +x le*
+
+printf "Moving files"
+
+mv le /usr/bin/
+mv le.sh /etc/rc.d/
+
+printf "Installtion complete, reboot your machine for changes to take effect"
