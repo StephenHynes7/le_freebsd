8cb3421cb7baa28e1d8c0e4fe247d09e7e3eb214
StephenHynes7 <stephen@logentries.com>
2013-09-16 11:34:05 +0100

    Update install_le.sh

--- install_le.sh
+++ install_le.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-printf "Downloading nessceary files"
+printf "Downloading nessceary files.\n"
 curl -O https://raw.github.com/logentries/le/master/le
 curl -O https://raw.github.com/StephenHynes7/le_freebsd/master/logentries
 chmod +x le
 chmod +x logentries
 
-printf "Moving files"
+printf "Moving files.\n"
 
 mv le /usr/bin/
 mv logentries /etc/rc.d/
 
 echo "logentries_enable=\"YES\"" >> /etc/rc.conf
 
-printf "Installtion complete, reboot your machine for changes to take effect"
+printf "Installtion complete, reboot your machine for changes to take effect.\n"
